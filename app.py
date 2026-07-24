7859a3d20ac31dec7ad36e155afb02ffeb9a0f31
nigmucode@gmail.com <nigmucode@gmail.com>
2026-07-25 05:08:51 +0530

    prod changes

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def download(filename):
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=2003, debug=True)
