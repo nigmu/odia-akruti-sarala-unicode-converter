411754db408a9eaf306729a6eb6c303fe1d17e56
nigmucode@gmail.com <nigmucode@gmail.com>
2026-07-25 22:24:20 +0530

    debugger turned off

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def download(filename):
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=2004, debug=True)
+    app.run(host="0.0.0.0", port=2004, debug=False)
